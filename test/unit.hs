4376881a51ea7b05d238796a2b1f9581e13aeddc
Lysxia <lysxia@gmail.com>
2018-02-25 09:19:11 -0500

    Add unit tests

--- test/unit.hs
+++ test/unit.hs
@@ -1,6 +1,8 @@
 {-# LANGUAGE DeriveGeneric #-}
 
+import Control.Applicative
 import Data.Semigroup
+import Data.Monoid (Sum(..))
 import Test.Tasty
 import Test.Tasty.HUnit
 
@@ -9,7 +11,7 @@ import Generic.Data
 import Generic.Data.Orphans ()
 
 data P a = P a a
-  deriving Generic
+  deriving (Generic, Generic1)
 
 type PTy a = a -> a -> Generically (P a)
 
@@ -22,6 +24,20 @@ p' = p
 pl :: PTy [Int]
 pl = p
 
+data P1 f a = P1 (f a) (f a)
+  deriving Generic1
+
+type PTy1 a = [a] -> [a] -> Generically1 (P1 []) a
+
+p1 :: PTy1 a
+p1 a b = Generically1 (P1 a b)
+
+p1' :: PTy1 Int
+p1' = p1
+
+pl1 :: PTy1 [Int]
+pl1 = p1
+
 main :: IO ()
 main = defaultMain test
 
@@ -29,7 +45,7 @@ test :: TestTree
 test = testGroup "unit"
   [ testGroup "Eq"
       [ testCase "(==)" $ p' 1 2 @?= p' 1 2
-      , testCase "(/=)" $ False @?= (p' 1 2 == p' 2 1)
+      , testCase "(/=)" $ False @?= (p' 1 2 == p' 1 1)
       ]
   , testGroup "Ord"
       [ testCase "compare" $ LT @?= compare (p' 1 2) (p' 2 1)
@@ -40,5 +56,27 @@ test = testGroup "unit"
       ]
   , testGroup "Monoid"
       [ testCase "mempty" $ pl [] [] @?= mempty
+      ]
+  , testGroup "Functor"
+      [ testCase "fmap" $ p1' [1] [2] @?= fmap (+ 1) (p1 [0] [1])
+      ]
+  , testGroup "Applicative"
+      [ testCase "pure" $ p1' [3] [3] @?= pure 3
+      , testCase "ap" $ p1' [1, 3] [2] @?= (p1 [id, (+2)] [(+2)] <*> p1 [1] [0])
+      ]
+  , testGroup "Alternative"
+      [ testCase "empty" $ p1' [] [] @?= empty
+      , testCase "(<|>)" $ p1' [1, 5] [2, 3] @?= (p1 [1] [2] <|> p1 [5] [3])
+      ]
+  , testGroup "Foldable"
+      [ testCase "foldMap" $ Sum 3 @?= foldMap Sum (p1' [1] [2])
+      , testCase "foldr" $ 3 @?= foldr (+) 0 (p1' [1] [2])
+      ]
+  , testGroup "Traversable"
+      [ testCase "traverse" $
+          [p1 [1] [2], p1 [1] [3], p1 [2] [2], p1 [2] [3]] @?=
+            traverse (\y -> [y, y+1]) (p1' [1] [2])
+      , testCase "sequenceA" $
+          [p1 [1] [2], p1 [2] [2]] @?= sequenceA (pl1 [[1, 2]] [[2]])
       ]
   ]
