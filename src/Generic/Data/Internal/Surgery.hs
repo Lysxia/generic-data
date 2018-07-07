5ddfb3a19335fde9b7b526ed658441d9551e52b9
Lysxia <lysxia@gmail.com>
2018-07-07 17:44:54 -0400

    Basic insert/remove fields

---
+++ src/Generic/Data/Internal/Surgery.hs
@@ -0,0 +1,69 @@
+{-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE ConstraintKinds #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE UndecidableInstances #-}
+
+-- | Operate on data types: insert/modify/delete fields and constructors.
+
+module Generic.Data.Internal.Surgery where
+
+import Data.Kind (Constraint)
+import Data.Type.Equality (type (==))
+import GHC.Generics
+import GHC.TypeLits
+
+import Generic.Data.Internal.Defun
+
+type family   Linearize (f :: k -> *) :: k -> *
+type instance Linearize (M1 d m f) = M1 d m (LinearizeSum f V1)
+
+type family   LinearizeSum (f :: k -> *) (tl :: k -> *) :: k -> *
+type instance LinearizeSum (f :+: g) tl = LinearizeSum f (LinearizeSum g tl)
+type instance LinearizeSum (M1 c m f) tl = M1 c m (LinearizeProduct f U1) :+: tl
+
+type family   LinearizeProduct (f :: k -> *) (tl :: k -> *) :: k -> *
+type instance LinearizeProduct (f :*: g) tl = LinearizeProduct f (LinearizeProduct g tl)
+type instance LinearizeProduct (M1 s m f) tl = M1 s m f :*: tl
+
+type family   FieldTypeAt (n :: Nat) (f :: k -> *) :: *
+type instance FieldTypeAt n (M1 i c f) = FieldTypeAt n f
+type instance FieldTypeAt n (f :*: g) = If (n == 0) (FieldTypeOf f) (FieldTypeAt (n-1) g)
+
+type family   FieldTypeOf (f :: k -> *) :: *
+type instance FieldTypeOf (M1 s m (K1 i a)) = a
+
+type family   RemoveField (n :: Nat) (f :: k -> *) :: k -> *
+type instance RemoveField n (M1 i m f) = M1 i m (RemoveField n f)
+type instance RemoveField n (f :*: g) = If (n == 0) g (f :*: RemoveField (n-1) g)
+
+class GRemoveField (n :: Nat) f where
+  gRemoveField :: f x -> (FieldTypeAt n f, RemoveField n f x)
+
+instance GRemoveField n f => GRemoveField n (M1 i c f) where
+  gRemoveField (M1 a) = M1 <$> gRemoveField @n a
+
+instance (If (n == 0) (() :: Constraint) (GRemoveField (n-1) g), IsBool (n == 0))
+  => GRemoveField n (M1 s m (K1 i t) :*: g) where
+  gRemoveField (a@(M1 (K1 t)) :*: b) = _If @(n == 0)
+    (t, b)
+    ((a :*:) <$> gRemoveField @(n-1) b)
+
+class GInsertField (n :: Nat) f where
+  gInsertField :: RemoveField n f x -> FieldTypeAt n f -> f x
+
+instance GInsertField n f => GInsertField n (M1 i c f) where
+  gInsertField (M1 a) t = M1 (gInsertField @n a t)
+
+instance (If (n == 0) (() :: Constraint) (GInsertField (n-1) g), IsBool (n == 0))
+  => GInsertField n (M1 s m (K1 i t) :*: g) where
+  gInsertField ab t = _If @(n == 0)
+    (M1 (K1 t) :*: ab)
+    (let a :*: b = ab in a :*: gInsertField @(n-1) b t)
