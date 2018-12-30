4617fcb3260f76b57da6522990b22a063738f45f
Lysxia <lysxia@gmail.com>
2018-12-30 16:05:30 +0100

    Add tests for extra microsurgeries

--- test/microsurgery.hs
+++ test/microsurgery.hs
@@ -1,5 +1,9 @@
 {-# LANGUAGE
-    DeriveGeneric #-}
+    DeriveGeneric,
+    DataKinds,
+    TypeApplications #-}
+
+-- @DataKinds@ and @TypeApplications@ for @renameFields@ and @renameConstrs@
 
 import GHC.Generics -- We need to import the constructors for Coercible to resolve
 import Test.Tasty
@@ -15,10 +19,21 @@ newtype T = T { _unT :: Int } deriving Generic
 instance Show T where
   showsPrec n = gshowsPrec n . derecordify . toData
 
+newtype U = U { _unU :: Int } deriving Generic
+
+instance Show U where
+  showsPrec n =
+    gshowsPrec n
+      . renameFields @(SRename '[ '("_unU", "unV")] SError)
+      . renameConstrs @(SConst "V")
+      . typeage  -- doesn't change anything, just a sanity check.
+      . toData
+
 main :: IO ()
 main = defaultMain test
 
 test :: TestTree
 test = testGroup "microsurgery"
-  [ testCase "Show" $ "T 3" @?= show (T 3)
+  [ testCase "Show T" $ "T 3" @?= show (T 3)
+  , testCase "Show U" $ "V {unV = 3}" @?= show (U 3)
   ]
